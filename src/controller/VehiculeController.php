b87825178bc415c75bbbbaf0dff0164a9c516e31
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-05-25 16:52:58 +0200

    ajout a la bdd (new)

--- src/controller/VehiculeController.php
+++ src/controller/VehiculeController.php
@@ -27,9 +27,15 @@ class VehiculeController extends AbstractController{
         echo self::getTwig()->render('vehicule/create.html');
     }
 
-    
     public static function new() {
-        echo self::getTwig()->render('vehicule/new.html');
+        $vehicule = new Vehicule;
+        $vehicule->setMarque($_POST['marque']);
+        $vehicule->setModele($_POST['modele']);
+        $vehicule->setCouleur($_POST['couleur']);
+        $vehicule->setImmatriculation($_POST['immatriculation']);
+        $vehicule->store();
+
+        self::index();
     }
 
     
